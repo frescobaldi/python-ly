2803d02a4cc683f8139f768177de28d06bf4ad88
Wilbert Berendsen <wbsoft@xs4all.nl>
2015-01-23 19:34:47 +0100

    doc of module

--- ly/__init__.py
+++ ly/__init__.py
@@ -20,8 +20,36 @@
 """
 A package of modules dealing with LilyPond and the LilyPond format.
 
-This package does not depend on PyQt4.
-Some modules however expect the _ (underscore) builtin to translate strings.
+The ly module supports both Python2 and Python3. This is a short description
+of some modules:
+
+  * ly.slexer: generic tools to build parsers using regular expressions
+  * ly.node: a generic list-like node object to build tree structures with
+  * ly.document: a tokenized text document (LilyPond file)
+  * ly.docinfo: harvests and caches various information from a LilyPond document
+  * ly.lex: a parser for LilyPond, Scheme, and other formats, using slexer
+  * ly.music: a tree structure of the contents of a document
+  * ly.pitch: functions for translating, transposing etc
+  * ly.rhythm: functions dealing with rhyhtm
+  * ly.indent: indent LilyPond text
+  * ly.reformat: format LilyPond text
+  * ly.dom: (deprecated) tree structure to build LilyPond text from
+  * ly.words: words for highlighting and autocompletion
+  * ly.data: layout objects, properties, interfaces, font glyphs etc extracted
+    from LilyPond
+  * ly.cli: the implementation of the command-line 'ly' script
+
+A LilyPond document (source file) is held by a ly.document.Document. The
+Document class automatically parses and tokenizes the text, also when its
+contents are changed.
+
+A music tree can be built from a document using the ly.music module. 
+In the near future, music trees can be built from scratch and also generate
+LilyPond output from scratch. At that moment, ly.dom is deprecated.
+
+The functions in ly.pitch, such as transpose and translate currently access
+the tokens in the document, but in the future they will work on the music tree.
+
 
 """
 
