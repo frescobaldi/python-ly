5493e473fc41a50dc60cd7182b6fcd9ae864f8eb
Wilbert Berendsen <wbsoft@xs4all.nl>
2016-03-30 10:37:44 +0200

    hopefully fix #59

--- ly/cli/command.py
+++ ly/cli/command.py
@@ -188,7 +188,7 @@ class musicxml(_export_command):
     def run(self, opts, cursor, output):
         import ly.musicxml
         writer = ly.musicxml.writer()
-        writer.parse_text(cursor.document.plaintext())
+        writer.parse_document(cursor.document)
         xml = writer.musicxml()
         if self.output:
             filename = self.output
