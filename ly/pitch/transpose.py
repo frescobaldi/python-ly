ceddb9d0153dd3af72c11d2bc27a6c7de9001f10
Wilbert Berendsen <wbsoft@xs4all.nl>
2016-04-19 16:56:04 +0200

    add new simplifier transposer

--- ly/pitch/transpose.py
+++ ly/pitch/transpose.py
@@ -70,6 +70,41 @@ class Transposer(object):
             pitch.note = note
             
             
+class Simplifier(Transposer):
+    """Make complicated accidentals simpler by substituting naturals where possible.
+    
+    """
+    def __init__(self, scale=None):
+        if scale is not None:
+            self.scale = scale
+    
+    def transpose(self, pitch):
+        if pitch.alter == 1:
+            doct, note = divmod(pitch.note + 1, 7)
+            pitch.alter -= doct * 6 + self.scale[note] - self.scale[pitch.note]
+            pitch.octave += doct
+            pitch.note = note
+        elif pitch.alter == -1:
+            doct, note = divmod(pitch.note - 1, 7)
+            pitch.alter += doct * -6 + self.scale[pitch.note] - self.scale[note]
+            pitch.octave += doct
+            pitch.note = note
+        if pitch.alter == Fraction(1, 2):
+            doct, note = divmod(pitch.note + 1, 7)
+            alter = doct * 6 + self.scale[note] - self.scale[pitch.note]
+            if alter == Fraction(1, 2):
+                pitch.alter = 0
+                pitch.octave += doct
+                pitch.note = note
+        elif pitch.alter == Fraction(-1, 2):
+            doct, note = divmod(pitch.note - 1, 7)
+            alter = doct * 6 + self.scale[pitch.note] - self.scale[note]
+            if alter == Fraction(1, 2):
+                pitch.alter = 0
+                pitch.octave += doct
+                pitch.note = note
+
+
 class ModeShifter(Transposer):
     """
     Shift pitches to optional mode/scale.
