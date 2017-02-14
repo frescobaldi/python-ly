cd5847723cedcdefc66db291063a9c0fe7e6b143
Endre Oma <endre.88.oma@gmail.com>
2017-02-14 23:22:54 +0100

    Add tests for the slur enhancement

--- tests/test_xml.py
+++ tests/test_xml.py
@@ -30,6 +30,8 @@ def test_dynamics():
 def test_tuplet():
     compare_output('tuplet')
 
+def test_merge_voice_slurs():
+    compare_output('merge_voice_slurs')
 
 def ly_to_xml(filename):
     """Read Lilypond file and return XML string."""
