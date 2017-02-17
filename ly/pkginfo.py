093f4e0ea04fa6cfb72a8d3dc222cee6856256ff
Wilbert Berendsen <wbsoft@xs4all.nl>
2017-02-17 07:19:49 +0100

    version bump

--- ly/pkginfo.py
+++ ly/pkginfo.py
@@ -31,7 +31,7 @@ from __future__ import unicode_literals
 name = "python-ly"
 
 #: the current version
-version = "0.9.4"
+version = "0.9.5"
 
 #: short description
 description = "Tool and library for manipulating LilyPond files"
