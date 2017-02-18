b5941a00cb52ad1f063428f416655886053d1517
Endre Oma <endre.88.oma@gmail.com>
2017-02-18 18:57:41 +0100

    MusicXML: ParseSource.Command() should accept \breathe

--- ly/musicxml/lymus2musxml.py
+++ ly/musicxml/lymus2musxml.py
@@ -497,6 +497,8 @@ class ParseSource():
             if self.tupl_span:
                 self.mediator.unset_tuplspan_dur()
                 self.tupl_span = False
+        elif command.token == '\\breathe':
+            self.mediator.new_articulation(command.token)
         else:
             if command.token not in excls:
                 print("Unknown command:", command.token)
