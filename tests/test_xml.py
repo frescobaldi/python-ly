8f274fdf5b45b5e801a6305641f848462528427c
felipperoza <felippesroza@gmail.com>
2017-07-20 15:03:41 -0300

    added test file and resulting xml

--- tests/test_xml.py
+++ tests/test_xml.py
@@ -31,6 +31,10 @@ def test_tuplet():
     compare_output('tuplet')
 
 
+def test_church():
+    compare_output('church_modes')
+
+
 def ly_to_xml(filename):
     """Read Lilypond file and return XML string."""
     writer = ly.musicxml.writer()
---
+++ tests/test_xml_files/church_modes.ly
@@ -0,0 +1,12 @@
+\version "2.18.2"
+
+\score {
+  \relative c' {
+  \key d \major
+  a b c d |
+
+  \key d \dorian
+  a b c d
+  }
+  \layout {}
+}
