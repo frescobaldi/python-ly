cbca39a97e9fb825ccf960e9e2bbcef255ad513b
Federico Bruni <fede@inventati.org>
2026-03-30 18:28:47 +0200

    Add the SPDX license identifier

--- ly/__init__.py
+++ ly/__init__.py
@@ -1,6 +1,7 @@
 # This file is part of python-ly, https://pypi.python.org/pypi/python-ly
 #
-# Copyright (c) 2008 - 2015 by Wilbert Berendsen
+# SPDX-FileCopyrightText: 2008 - 2015 Wilbert Berendsen
+# SPDX-License-Identifier: GPL-3.0-or-later
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
